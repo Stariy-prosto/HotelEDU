154c83ebb1ea69fd8b438dfc4edc26e96f5b7c97
Старый Данил <danil_stariy@outlook.com>
2023-05-03 12:31:43 +0800

    Обнавлена форма

--- HotelEDU/SplashScreenForm.Designer.cs
+++ HotelEDU/SplashScreenForm.Designer.cs
@@ -31,40 +31,58 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreenForm));
             progressBar1 = new ProgressBar();
             pictureBox1 = new PictureBox();
+            tableLayoutPanel1 = new TableLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // progressBar1
             // 
-            progressBar1.Dock = DockStyle.Bottom;
-            progressBar1.Location = new Point(0, 307);
+            progressBar1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            progressBar1.Location = new Point(3, 303);
             progressBar1.MarqueeAnimationSpeed = 50;
             progressBar1.Name = "progressBar1";
-            progressBar1.Size = new Size(530, 23);
+            progressBar1.Size = new Size(524, 24);
             progressBar1.Style = ProgressBarStyle.Marquee;
             progressBar1.TabIndex = 0;
             progressBar1.UseWaitCursor = true;
             // 
             // pictureBox1
             // 
-            pictureBox1.Dock = DockStyle.Top;
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Location = new Point(3, 3);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(530, 318);
+            pictureBox1.Size = new Size(524, 294);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
             pictureBox1.UseWaitCursor = true;
             // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
+            tableLayoutPanel1.Controls.Add(progressBar1, 0, 1);
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 91F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 9F));
+            tableLayoutPanel1.Size = new Size(530, 330);
+            tableLayoutPanel1.TabIndex = 2;
+            tableLayoutPanel1.UseWaitCursor = true;
+            // 
             // SplashScreenForm
             // 
-            AutoScaleDimensions = new SizeF(6F, 13F);
-            AutoScaleMode = AutoScaleMode.Font;
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            AutoSize = true;
             ClientSize = new Size(530, 330);
             ControlBox = false;
-            Controls.Add(progressBar1);
-            Controls.Add(pictureBox1);
+            Controls.Add(tableLayoutPanel1);
             FormBorderStyle = FormBorderStyle.None;
             MaximizeBox = false;
             MinimizeBox = false;
@@ -73,6 +91,7 @@
             StartPosition = FormStartPosition.CenterScreen;
             UseWaitCursor = true;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -80,5 +99,6 @@
 
         private ProgressBar progressBar1;
         private PictureBox pictureBox1;
+        private TableLayoutPanel tableLayoutPanel1;
     }
 }
