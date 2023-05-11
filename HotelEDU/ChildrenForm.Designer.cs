19a5efac377201866df105b784f5f0a5daad12ab
Stariy <djplaymmen@gmail.com>
2023-05-11 21:49:57 +0800

    Черновой вариант формы Children с элементами

---
+++ HotelEDU/ChildrenForm.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace HotelEDU
+{
+    partial class ChildrenForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChildrenForm));
+            this.OKButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Age1Label = new System.Windows.Forms.Label();
+            this.Age1TextBox = new System.Windows.Forms.TextBox();
+            this.Age2Label = new System.Windows.Forms.Label();
+            this.Age2TextBox = new System.Windows.Forms.TextBox();
+            this.Age3Label = new System.Windows.Forms.Label();
+            this.Age3TextBox = new System.Windows.Forms.TextBox();
+            this.AgeLabel = new System.Windows.Forms.Label();
+            this.Children1Label = new System.Windows.Forms.Label();
+            this.Children1TextBox = new System.Windows.Forms.TextBox();
+            this.Children2TextBox = new System.Windows.Forms.TextBox();
+            this.Children2Label = new System.Windows.Forms.Label();
+            this.Children3TextBox = new System.Windows.Forms.TextBox();
+            this.Children3Label = new System.Windows.Forms.Label();
+            this.Children4TextBox = new System.Windows.Forms.TextBox();
+            this.Children4Label = new System.Windows.Forms.Label();
+            this.Children5TextBox = new System.Windows.Forms.TextBox();
+            this.Children5Label = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OKButton
+            // 
+            this.OKButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.OKButton.Location = new System.Drawing.Point(0, 106);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(80, 23);
+            this.OKButton.TabIndex = 0;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.CloseButton.Location = new System.Drawing.Point(0, 129);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(80, 23);
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.DeleteButton.Location = new System.Drawing.Point(0, 83);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(80, 23);
+            this.DeleteButton.TabIndex = 2;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.DeleteButton);
+            this.panel1.Controls.Add(this.OKButton);
+            this.panel1.Controls.Add(this.CloseButton);
+            this.panel1.Location = new System.Drawing.Point(227, 8);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(80, 152);
+            this.panel1.TabIndex = 3;
+            // 
+            // Age1Label
+            // 
+            this.Age1Label.AutoSize = true;
+            this.Age1Label.Location = new System.Drawing.Point(18, 16);
+            this.Age1Label.Name = "Age1Label";
+            this.Age1Label.Size = new System.Drawing.Size(22, 13);
+            this.Age1Label.TabIndex = 4;
+            this.Age1Label.Text = "0-3";
+            // 
+            // Age1TextBox
+            // 
+            this.Age1TextBox.Location = new System.Drawing.Point(46, 13);
+            this.Age1TextBox.Name = "Age1TextBox";
+            this.Age1TextBox.Size = new System.Drawing.Size(30, 20);
+            this.Age1TextBox.TabIndex = 5;
+            // 
+            // Age2Label
+            // 
+            this.Age2Label.AutoSize = true;
+            this.Age2Label.Location = new System.Drawing.Point(18, 42);
+            this.Age2Label.Name = "Age2Label";
+            this.Age2Label.Size = new System.Drawing.Size(22, 13);
+            this.Age2Label.TabIndex = 6;
+            this.Age2Label.Text = "4-7";
+            // 
+            // Age2TextBox
+            // 
+            this.Age2TextBox.Location = new System.Drawing.Point(46, 39);
+            this.Age2TextBox.Name = "Age2TextBox";
+            this.Age2TextBox.Size = new System.Drawing.Size(30, 20);
+            this.Age2TextBox.TabIndex = 7;
+            // 
+            // Age3Label
+            // 
+            this.Age3Label.AutoSize = true;
+            this.Age3Label.Location = new System.Drawing.Point(12, 68);
+            this.Age3Label.Name = "Age3Label";
+            this.Age3Label.Size = new System.Drawing.Size(28, 13);
+            this.Age3Label.TabIndex = 8;
+            this.Age3Label.Text = "8-10";
+            // 
+            // Age3TextBox
+            // 
+            this.Age3TextBox.Location = new System.Drawing.Point(46, 65);
+            this.Age3TextBox.Name = "Age3TextBox";
+            this.Age3TextBox.Size = new System.Drawing.Size(30, 20);
+            this.Age3TextBox.TabIndex = 9;
+            // 
+            // AgeLabel
+            // 
+            this.AgeLabel.AutoSize = true;
+            this.AgeLabel.Location = new System.Drawing.Point(111, 16);
+            this.AgeLabel.Name = "AgeLabel";
+            this.AgeLabel.Size = new System.Drawing.Size(26, 13);
+            this.AgeLabel.TabIndex = 10;
+            this.AgeLabel.Text = "Age";
+            // 
+            // Children1Label
+            // 
+            this.Children1Label.AutoSize = true;
+            this.Children1Label.Location = new System.Drawing.Point(111, 39);
+            this.Children1Label.Name = "Children1Label";
+            this.Children1Label.Size = new System.Drawing.Size(54, 13);
+            this.Children1Label.TabIndex = 11;
+            this.Children1Label.Text = "Children 1";
+            // 
+            // Children1TextBox
+            // 
+            this.Children1TextBox.Location = new System.Drawing.Point(171, 36);
+            this.Children1TextBox.Name = "Children1TextBox";
+            this.Children1TextBox.Size = new System.Drawing.Size(50, 20);
+            this.Children1TextBox.TabIndex = 12;
+            // 
+            // Children2TextBox
+            // 
+            this.Children2TextBox.Location = new System.Drawing.Point(171, 62);
+            this.Children2TextBox.Name = "Children2TextBox";
+            this.Children2TextBox.Size = new System.Drawing.Size(50, 20);
+            this.Children2TextBox.TabIndex = 14;
+            // 
+            // Children2Label
+            // 
+            this.Children2Label.AutoSize = true;
+            this.Children2Label.Location = new System.Drawing.Point(111, 65);
+            this.Children2Label.Name = "Children2Label";
+            this.Children2Label.Size = new System.Drawing.Size(54, 13);
+            this.Children2Label.TabIndex = 13;
+            this.Children2Label.Text = "Children 2";
+            // 
+            // Children3TextBox
+            // 
+            this.Children3TextBox.Location = new System.Drawing.Point(171, 88);
+            this.Children3TextBox.Name = "Children3TextBox";
+            this.Children3TextBox.Size = new System.Drawing.Size(50, 20);
+            this.Children3TextBox.TabIndex = 16;
+            // 
+            // Children3Label
+            // 
+            this.Children3Label.AutoSize = true;
+            this.Children3Label.Location = new System.Drawing.Point(111, 91);
+            this.Children3Label.Name = "Children3Label";
+            this.Children3Label.Size = new System.Drawing.Size(54, 13);
+            this.Children3Label.TabIndex = 15;
+            this.Children3Label.Text = "Children 3";
+            // 
+            // Children4TextBox
+            // 
+            this.Children4TextBox.Location = new System.Drawing.Point(171, 114);
+            this.Children4TextBox.Name = "Children4TextBox";
+            this.Children4TextBox.Size = new System.Drawing.Size(50, 20);
+            this.Children4TextBox.TabIndex = 18;
+            // 
+            // Children4Label
+            // 
+            this.Children4Label.AutoSize = true;
+            this.Children4Label.Location = new System.Drawing.Point(111, 117);
+            this.Children4Label.Name = "Children4Label";
+            this.Children4Label.Size = new System.Drawing.Size(54, 13);
+            this.Children4Label.TabIndex = 17;
+            this.Children4Label.Text = "Children 4";
+            // 
+            // Children5TextBox
+            // 
+            this.Children5TextBox.Location = new System.Drawing.Point(171, 140);
+            this.Children5TextBox.Name = "Children5TextBox";
+            this.Children5TextBox.Size = new System.Drawing.Size(50, 20);
+            this.Children5TextBox.TabIndex = 20;
+            // 
+            // Children5Label
+            // 
+            this.Children5Label.AutoSize = true;
+            this.Children5Label.Location = new System.Drawing.Point(111, 143);
+            this.Children5Label.Name = "Children5Label";
+            this.Children5Label.Size = new System.Drawing.Size(54, 13);
+            this.Children5Label.TabIndex = 19;
+            this.Children5Label.Text = "Children 5";
+            // 
+            // ChildrenForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 166);
+            this.Controls.Add(this.Children5TextBox);
+            this.Controls.Add(this.Children5Label);
+            this.Controls.Add(this.Children4TextBox);
+            this.Controls.Add(this.Children4Label);
+            this.Controls.Add(this.Children3TextBox);
+            this.Controls.Add(this.Children3Label);
+            this.Controls.Add(this.Children2TextBox);
+            this.Controls.Add(this.Children2Label);
+            this.Controls.Add(this.Children1TextBox);
+            this.Controls.Add(this.Children1Label);
+            this.Controls.Add(this.AgeLabel);
+            this.Controls.Add(this.Age3TextBox);
+            this.Controls.Add(this.Age3Label);
+            this.Controls.Add(this.Age2TextBox);
+            this.Controls.Add(this.Age2Label);
+            this.Controls.Add(this.Age1TextBox);
+            this.Controls.Add(this.Age1Label);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ChildrenForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ChildrenForm";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OKButton;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label Age1Label;
+        private System.Windows.Forms.TextBox Age1TextBox;
+        private System.Windows.Forms.Label Age2Label;
+        private System.Windows.Forms.TextBox Age2TextBox;
+        private System.Windows.Forms.Label Age3Label;
+        private System.Windows.Forms.TextBox Age3TextBox;
+        private System.Windows.Forms.Label AgeLabel;
+        private System.Windows.Forms.Label Children1Label;
+        private System.Windows.Forms.TextBox Children1TextBox;
+        private System.Windows.Forms.TextBox Children2TextBox;
+        private System.Windows.Forms.Label Children2Label;
+        private System.Windows.Forms.TextBox Children3TextBox;
+        private System.Windows.Forms.Label Children3Label;
+        private System.Windows.Forms.TextBox Children4TextBox;
+        private System.Windows.Forms.Label Children4Label;
+        private System.Windows.Forms.TextBox Children5TextBox;
+        private System.Windows.Forms.Label Children5Label;
+    }
+}
