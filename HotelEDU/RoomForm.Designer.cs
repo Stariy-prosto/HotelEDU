d8b6b268707b8747d9fa4330005ef73ca6b1579c
Stariy <djplaymmen@gmail.com>
2023-05-12 00:15:42 +0800

    Черновой вариант RoomForm с элементами

---
+++ HotelEDU/RoomForm.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace HotelEDU
+{
+    partial class RoomForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RoomForm));
+            this.RoomNoLabel = new System.Windows.Forms.Label();
+            this.RoomNoTextBox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.RoomTypeLabel = new System.Windows.Forms.Label();
+            this.DescriptionTextBox = new System.Windows.Forms.TextBox();
+            this.DescriptionLabel = new System.Windows.Forms.Label();
+            this.MaxOccupancyTextBox = new System.Windows.Forms.TextBox();
+            this.MaxOccupancyLabel = new System.Windows.Forms.Label();
+            this.SmokingPreferenceTextBox = new System.Windows.Forms.TextBox();
+            this.SmokingPreferenceLabel = new System.Windows.Forms.Label();
+            this.FeaturesTextBox = new System.Windows.Forms.TextBox();
+            this.FeaturesLabel = new System.Windows.Forms.Label();
+            this.ComponentRoomsTextBox = new System.Windows.Forms.TextBox();
+            this.ComponentRoomsLabel = new System.Windows.Forms.Label();
+            this.ConnectingRoomsTextBox = new System.Windows.Forms.TextBox();
+            this.ConnectingRoomsLabel = new System.Windows.Forms.Label();
+            this.SquareUnitsTextBox = new System.Windows.Forms.TextBox();
+            this.SquareUnitsLabel = new System.Windows.Forms.Label();
+            this.PhoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.PhoneNumberLabel = new System.Windows.Forms.Label();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RoomNoLabel
+            // 
+            this.RoomNoLabel.AutoSize = true;
+            this.RoomNoLabel.Location = new System.Drawing.Point(63, 14);
+            this.RoomNoLabel.Name = "RoomNoLabel";
+            this.RoomNoLabel.Size = new System.Drawing.Size(52, 13);
+            this.RoomNoLabel.TabIndex = 0;
+            this.RoomNoLabel.Text = "Room No";
+            // 
+            // RoomNoTextBox
+            // 
+            this.RoomNoTextBox.Location = new System.Drawing.Point(121, 11);
+            this.RoomNoTextBox.Name = "RoomNoTextBox";
+            this.RoomNoTextBox.Size = new System.Drawing.Size(100, 20);
+            this.RoomNoTextBox.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(121, 37);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // RoomTypeLabel
+            // 
+            this.RoomTypeLabel.AutoSize = true;
+            this.RoomTypeLabel.Location = new System.Drawing.Point(53, 40);
+            this.RoomTypeLabel.Name = "RoomTypeLabel";
+            this.RoomTypeLabel.Size = new System.Drawing.Size(62, 13);
+            this.RoomTypeLabel.TabIndex = 2;
+            this.RoomTypeLabel.Text = "Room Type";
+            // 
+            // DescriptionTextBox
+            // 
+            this.DescriptionTextBox.Location = new System.Drawing.Point(121, 63);
+            this.DescriptionTextBox.Name = "DescriptionTextBox";
+            this.DescriptionTextBox.Size = new System.Drawing.Size(150, 20);
+            this.DescriptionTextBox.TabIndex = 5;
+            // 
+            // DescriptionLabel
+            // 
+            this.DescriptionLabel.AutoSize = true;
+            this.DescriptionLabel.Location = new System.Drawing.Point(55, 66);
+            this.DescriptionLabel.Name = "DescriptionLabel";
+            this.DescriptionLabel.Size = new System.Drawing.Size(60, 13);
+            this.DescriptionLabel.TabIndex = 4;
+            this.DescriptionLabel.Text = "Description";
+            // 
+            // MaxOccupancyTextBox
+            // 
+            this.MaxOccupancyTextBox.Location = new System.Drawing.Point(121, 89);
+            this.MaxOccupancyTextBox.Name = "MaxOccupancyTextBox";
+            this.MaxOccupancyTextBox.Size = new System.Drawing.Size(100, 20);
+            this.MaxOccupancyTextBox.TabIndex = 7;
+            // 
+            // MaxOccupancyLabel
+            // 
+            this.MaxOccupancyLabel.AutoSize = true;
+            this.MaxOccupancyLabel.Location = new System.Drawing.Point(27, 92);
+            this.MaxOccupancyLabel.Name = "MaxOccupancyLabel";
+            this.MaxOccupancyLabel.Size = new System.Drawing.Size(88, 13);
+            this.MaxOccupancyLabel.TabIndex = 6;
+            this.MaxOccupancyLabel.Text = "Max. Occupancy";
+            // 
+            // SmokingPreferenceTextBox
+            // 
+            this.SmokingPreferenceTextBox.Location = new System.Drawing.Point(121, 115);
+            this.SmokingPreferenceTextBox.Name = "SmokingPreferenceTextBox";
+            this.SmokingPreferenceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.SmokingPreferenceTextBox.TabIndex = 9;
+            // 
+            // SmokingPreferenceLabel
+            // 
+            this.SmokingPreferenceLabel.AutoSize = true;
+            this.SmokingPreferenceLabel.Location = new System.Drawing.Point(12, 118);
+            this.SmokingPreferenceLabel.Name = "SmokingPreferenceLabel";
+            this.SmokingPreferenceLabel.Size = new System.Drawing.Size(103, 13);
+            this.SmokingPreferenceLabel.TabIndex = 8;
+            this.SmokingPreferenceLabel.Text = "Smoking Preference";
+            // 
+            // FeaturesTextBox
+            // 
+            this.FeaturesTextBox.Location = new System.Drawing.Point(121, 141);
+            this.FeaturesTextBox.Name = "FeaturesTextBox";
+            this.FeaturesTextBox.Size = new System.Drawing.Size(150, 20);
+            this.FeaturesTextBox.TabIndex = 11;
+            // 
+            // FeaturesLabel
+            // 
+            this.FeaturesLabel.AutoSize = true;
+            this.FeaturesLabel.Location = new System.Drawing.Point(67, 144);
+            this.FeaturesLabel.Name = "FeaturesLabel";
+            this.FeaturesLabel.Size = new System.Drawing.Size(48, 13);
+            this.FeaturesLabel.TabIndex = 10;
+            this.FeaturesLabel.Text = "Features";
+            // 
+            // ComponentRoomsTextBox
+            // 
+            this.ComponentRoomsTextBox.Location = new System.Drawing.Point(121, 167);
+            this.ComponentRoomsTextBox.Name = "ComponentRoomsTextBox";
+            this.ComponentRoomsTextBox.Size = new System.Drawing.Size(150, 20);
+            this.ComponentRoomsTextBox.TabIndex = 13;
+            // 
+            // ComponentRoomsLabel
+            // 
+            this.ComponentRoomsLabel.AutoSize = true;
+            this.ComponentRoomsLabel.Location = new System.Drawing.Point(18, 170);
+            this.ComponentRoomsLabel.Name = "ComponentRoomsLabel";
+            this.ComponentRoomsLabel.Size = new System.Drawing.Size(97, 13);
+            this.ComponentRoomsLabel.TabIndex = 12;
+            this.ComponentRoomsLabel.Text = "Component Rooms";
+            // 
+            // ConnectingRoomsTextBox
+            // 
+            this.ConnectingRoomsTextBox.Location = new System.Drawing.Point(121, 193);
+            this.ConnectingRoomsTextBox.Name = "ConnectingRoomsTextBox";
+            this.ConnectingRoomsTextBox.Size = new System.Drawing.Size(150, 20);
+            this.ConnectingRoomsTextBox.TabIndex = 15;
+            // 
+            // ConnectingRoomsLabel
+            // 
+            this.ConnectingRoomsLabel.AutoSize = true;
+            this.ConnectingRoomsLabel.Location = new System.Drawing.Point(18, 196);
+            this.ConnectingRoomsLabel.Name = "ConnectingRoomsLabel";
+            this.ConnectingRoomsLabel.Size = new System.Drawing.Size(97, 13);
+            this.ConnectingRoomsLabel.TabIndex = 14;
+            this.ConnectingRoomsLabel.Text = "Connecting Rooms";
+            // 
+            // SquareUnitsTextBox
+            // 
+            this.SquareUnitsTextBox.Location = new System.Drawing.Point(121, 219);
+            this.SquareUnitsTextBox.Name = "SquareUnitsTextBox";
+            this.SquareUnitsTextBox.Size = new System.Drawing.Size(150, 20);
+            this.SquareUnitsTextBox.TabIndex = 17;
+            // 
+            // SquareUnitsLabel
+            // 
+            this.SquareUnitsLabel.AutoSize = true;
+            this.SquareUnitsLabel.Location = new System.Drawing.Point(47, 222);
+            this.SquareUnitsLabel.Name = "SquareUnitsLabel";
+            this.SquareUnitsLabel.Size = new System.Drawing.Size(68, 13);
+            this.SquareUnitsLabel.TabIndex = 16;
+            this.SquareUnitsLabel.Text = "Square Units";
+            // 
+            // PhoneNumberTextBox
+            // 
+            this.PhoneNumberTextBox.Location = new System.Drawing.Point(121, 245);
+            this.PhoneNumberTextBox.Name = "PhoneNumberTextBox";
+            this.PhoneNumberTextBox.Size = new System.Drawing.Size(150, 20);
+            this.PhoneNumberTextBox.TabIndex = 19;
+            // 
+            // PhoneNumberLabel
+            // 
+            this.PhoneNumberLabel.AutoSize = true;
+            this.PhoneNumberLabel.Location = new System.Drawing.Point(37, 248);
+            this.PhoneNumberLabel.Name = "PhoneNumberLabel";
+            this.PhoneNumberLabel.Size = new System.Drawing.Size(78, 13);
+            this.PhoneNumberLabel.TabIndex = 18;
+            this.PhoneNumberLabel.Text = "Phone Number";
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(196, 271);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(75, 23);
+            this.CloseButton.TabIndex = 20;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            // 
+            // RoomForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 300);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.PhoneNumberTextBox);
+            this.Controls.Add(this.PhoneNumberLabel);
+            this.Controls.Add(this.SquareUnitsTextBox);
+            this.Controls.Add(this.SquareUnitsLabel);
+            this.Controls.Add(this.ConnectingRoomsTextBox);
+            this.Controls.Add(this.ConnectingRoomsLabel);
+            this.Controls.Add(this.ComponentRoomsTextBox);
+            this.Controls.Add(this.ComponentRoomsLabel);
+            this.Controls.Add(this.FeaturesTextBox);
+            this.Controls.Add(this.FeaturesLabel);
+            this.Controls.Add(this.SmokingPreferenceTextBox);
+            this.Controls.Add(this.SmokingPreferenceLabel);
+            this.Controls.Add(this.MaxOccupancyTextBox);
+            this.Controls.Add(this.MaxOccupancyLabel);
+            this.Controls.Add(this.DescriptionTextBox);
+            this.Controls.Add(this.DescriptionLabel);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.RoomTypeLabel);
+            this.Controls.Add(this.RoomNoTextBox);
+            this.Controls.Add(this.RoomNoLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "RoomForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Room";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label RoomNoLabel;
+        private System.Windows.Forms.TextBox RoomNoTextBox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label RoomTypeLabel;
+        private System.Windows.Forms.TextBox DescriptionTextBox;
+        private System.Windows.Forms.Label DescriptionLabel;
+        private System.Windows.Forms.TextBox MaxOccupancyTextBox;
+        private System.Windows.Forms.Label MaxOccupancyLabel;
+        private System.Windows.Forms.TextBox SmokingPreferenceTextBox;
+        private System.Windows.Forms.Label SmokingPreferenceLabel;
+        private System.Windows.Forms.TextBox FeaturesTextBox;
+        private System.Windows.Forms.Label FeaturesLabel;
+        private System.Windows.Forms.TextBox ComponentRoomsTextBox;
+        private System.Windows.Forms.Label ComponentRoomsLabel;
+        private System.Windows.Forms.TextBox ConnectingRoomsTextBox;
+        private System.Windows.Forms.Label ConnectingRoomsLabel;
+        private System.Windows.Forms.TextBox SquareUnitsTextBox;
+        private System.Windows.Forms.Label SquareUnitsLabel;
+        private System.Windows.Forms.TextBox PhoneNumberTextBox;
+        private System.Windows.Forms.Label PhoneNumberLabel;
+        private System.Windows.Forms.Button CloseButton;
+    }
+}
