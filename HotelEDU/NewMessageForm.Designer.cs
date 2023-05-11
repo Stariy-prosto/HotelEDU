0d93ef94b03b61ba3a9586b16c1844d0a6f36d4a
Stariy <djplaymmen@gmail.com>
2023-05-12 01:10:38 +0800

    Черновой вариант New Message с элементами

---
+++ HotelEDU/NewMessageForm.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace HotelEDU
+{
+    partial class NewMessageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewMessageForm));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.MessageForGroupBox = new System.Windows.Forms.GroupBox();
+            this.ForNameLabel = new System.Windows.Forms.Label();
+            this.ForNameTextBox = new System.Windows.Forms.TextBox();
+            this.LanguageTextBox = new System.Windows.Forms.TextBox();
+            this.LanguageLabel = new System.Windows.Forms.Label();
+            this.LanguageButton = new System.Windows.Forms.Button();
+            this.MessageFromGroupBox = new System.Windows.Forms.GroupBox();
+            this.FromNameTextBox = new System.Windows.Forms.TextBox();
+            this.FromCompanyTextBox = new System.Windows.Forms.TextBox();
+            this.FromNameLabel = new System.Windows.Forms.Label();
+            this.FromCompanyLabel = new System.Windows.Forms.Label();
+            this.FromFirstTextBox = new System.Windows.Forms.TextBox();
+            this.FromFirstLabel = new System.Windows.Forms.Label();
+            this.FromTitleTextBox = new System.Windows.Forms.TextBox();
+            this.FromTitleLabel = new System.Windows.Forms.Label();
+            this.FromTitleButton = new System.Windows.Forms.Button();
+            this.FromPhoneNoTextBox = new System.Windows.Forms.TextBox();
+            this.FromPhoneNoLabel = new System.Windows.Forms.Label();
+            this.MessageTextBox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.OKButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.MessageForGroupBox.SuspendLayout();
+            this.MessageFromGroupBox.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(479, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // MessageForGroupBox
+            // 
+            this.MessageForGroupBox.Controls.Add(this.LanguageButton);
+            this.MessageForGroupBox.Controls.Add(this.ForNameTextBox);
+            this.MessageForGroupBox.Controls.Add(this.LanguageTextBox);
+            this.MessageForGroupBox.Controls.Add(this.ForNameLabel);
+            this.MessageForGroupBox.Controls.Add(this.LanguageLabel);
+            this.MessageForGroupBox.Location = new System.Drawing.Point(12, 168);
+            this.MessageForGroupBox.Name = "MessageForGroupBox";
+            this.MessageForGroupBox.Size = new System.Drawing.Size(479, 52);
+            this.MessageForGroupBox.TabIndex = 1;
+            this.MessageForGroupBox.TabStop = false;
+            this.MessageForGroupBox.Text = "Message For";
+            // 
+            // ForNameLabel
+            // 
+            this.ForNameLabel.AutoSize = true;
+            this.ForNameLabel.Location = new System.Drawing.Point(5, 22);
+            this.ForNameLabel.Name = "ForNameLabel";
+            this.ForNameLabel.Size = new System.Drawing.Size(35, 13);
+            this.ForNameLabel.TabIndex = 2;
+            this.ForNameLabel.Text = "Name";
+            // 
+            // ForNameTextBox
+            // 
+            this.ForNameTextBox.Location = new System.Drawing.Point(46, 19);
+            this.ForNameTextBox.Name = "ForNameTextBox";
+            this.ForNameTextBox.Size = new System.Drawing.Size(150, 20);
+            this.ForNameTextBox.TabIndex = 3;
+            // 
+            // LanguageTextBox
+            // 
+            this.LanguageTextBox.Location = new System.Drawing.Point(323, 19);
+            this.LanguageTextBox.Name = "LanguageTextBox";
+            this.LanguageTextBox.Size = new System.Drawing.Size(109, 20);
+            this.LanguageTextBox.TabIndex = 5;
+            // 
+            // LanguageLabel
+            // 
+            this.LanguageLabel.AutoSize = true;
+            this.LanguageLabel.Location = new System.Drawing.Point(262, 22);
+            this.LanguageLabel.Name = "LanguageLabel";
+            this.LanguageLabel.Size = new System.Drawing.Size(55, 13);
+            this.LanguageLabel.TabIndex = 4;
+            this.LanguageLabel.Text = "Language";
+            // 
+            // LanguageButton
+            // 
+            this.LanguageButton.Location = new System.Drawing.Point(438, 17);
+            this.LanguageButton.Name = "LanguageButton";
+            this.LanguageButton.Size = new System.Drawing.Size(35, 23);
+            this.LanguageButton.TabIndex = 6;
+            this.LanguageButton.Text = "button1";
+            this.LanguageButton.UseVisualStyleBackColor = true;
+            // 
+            // MessageFromGroupBox
+            // 
+            this.MessageFromGroupBox.Controls.Add(this.FromPhoneNoTextBox);
+            this.MessageFromGroupBox.Controls.Add(this.FromPhoneNoLabel);
+            this.MessageFromGroupBox.Controls.Add(this.FromTitleButton);
+            this.MessageFromGroupBox.Controls.Add(this.FromTitleTextBox);
+            this.MessageFromGroupBox.Controls.Add(this.FromTitleLabel);
+            this.MessageFromGroupBox.Controls.Add(this.FromFirstTextBox);
+            this.MessageFromGroupBox.Controls.Add(this.FromFirstLabel);
+            this.MessageFromGroupBox.Controls.Add(this.FromNameTextBox);
+            this.MessageFromGroupBox.Controls.Add(this.FromCompanyTextBox);
+            this.MessageFromGroupBox.Controls.Add(this.FromNameLabel);
+            this.MessageFromGroupBox.Controls.Add(this.FromCompanyLabel);
+            this.MessageFromGroupBox.Location = new System.Drawing.Point(12, 226);
+            this.MessageFromGroupBox.Name = "MessageFromGroupBox";
+            this.MessageFromGroupBox.Size = new System.Drawing.Size(479, 100);
+            this.MessageFromGroupBox.TabIndex = 7;
+            this.MessageFromGroupBox.TabStop = false;
+            this.MessageFromGroupBox.Text = "Message From";
+            // 
+            // FromNameTextBox
+            // 
+            this.FromNameTextBox.Location = new System.Drawing.Point(46, 19);
+            this.FromNameTextBox.Name = "FromNameTextBox";
+            this.FromNameTextBox.Size = new System.Drawing.Size(150, 20);
+            this.FromNameTextBox.TabIndex = 3;
+            // 
+            // FromCompanyTextBox
+            // 
+            this.FromCompanyTextBox.Location = new System.Drawing.Point(323, 19);
+            this.FromCompanyTextBox.Name = "FromCompanyTextBox";
+            this.FromCompanyTextBox.Size = new System.Drawing.Size(150, 20);
+            this.FromCompanyTextBox.TabIndex = 5;
+            // 
+            // FromNameLabel
+            // 
+            this.FromNameLabel.AutoSize = true;
+            this.FromNameLabel.Location = new System.Drawing.Point(5, 22);
+            this.FromNameLabel.Name = "FromNameLabel";
+            this.FromNameLabel.Size = new System.Drawing.Size(35, 13);
+            this.FromNameLabel.TabIndex = 2;
+            this.FromNameLabel.Text = "Name";
+            // 
+            // FromCompanyLabel
+            // 
+            this.FromCompanyLabel.AutoSize = true;
+            this.FromCompanyLabel.Location = new System.Drawing.Point(262, 22);
+            this.FromCompanyLabel.Name = "FromCompanyLabel";
+            this.FromCompanyLabel.Size = new System.Drawing.Size(51, 13);
+            this.FromCompanyLabel.TabIndex = 4;
+            this.FromCompanyLabel.Text = "Company";
+            // 
+            // FromFirstTextBox
+            // 
+            this.FromFirstTextBox.Location = new System.Drawing.Point(46, 45);
+            this.FromFirstTextBox.Name = "FromFirstTextBox";
+            this.FromFirstTextBox.Size = new System.Drawing.Size(150, 20);
+            this.FromFirstTextBox.TabIndex = 8;
+            // 
+            // FromFirstLabel
+            // 
+            this.FromFirstLabel.AutoSize = true;
+            this.FromFirstLabel.Location = new System.Drawing.Point(14, 48);
+            this.FromFirstLabel.Name = "FromFirstLabel";
+            this.FromFirstLabel.Size = new System.Drawing.Size(26, 13);
+            this.FromFirstLabel.TabIndex = 7;
+            this.FromFirstLabel.Text = "First";
+            // 
+            // FromTitleTextBox
+            // 
+            this.FromTitleTextBox.Location = new System.Drawing.Point(46, 71);
+            this.FromTitleTextBox.Name = "FromTitleTextBox";
+            this.FromTitleTextBox.Size = new System.Drawing.Size(109, 20);
+            this.FromTitleTextBox.TabIndex = 10;
+            // 
+            // FromTitleLabel
+            // 
+            this.FromTitleLabel.AutoSize = true;
+            this.FromTitleLabel.Location = new System.Drawing.Point(14, 74);
+            this.FromTitleLabel.Name = "FromTitleLabel";
+            this.FromTitleLabel.Size = new System.Drawing.Size(27, 13);
+            this.FromTitleLabel.TabIndex = 9;
+            this.FromTitleLabel.Text = "Title";
+            // 
+            // FromTitleButton
+            // 
+            this.FromTitleButton.Location = new System.Drawing.Point(161, 69);
+            this.FromTitleButton.Name = "FromTitleButton";
+            this.FromTitleButton.Size = new System.Drawing.Size(35, 23);
+            this.FromTitleButton.TabIndex = 11;
+            this.FromTitleButton.Text = "button2";
+            this.FromTitleButton.UseVisualStyleBackColor = true;
+            // 
+            // FromPhoneNoTextBox
+            // 
+            this.FromPhoneNoTextBox.Location = new System.Drawing.Point(323, 45);
+            this.FromPhoneNoTextBox.Name = "FromPhoneNoTextBox";
+            this.FromPhoneNoTextBox.Size = new System.Drawing.Size(150, 20);
+            this.FromPhoneNoTextBox.TabIndex = 13;
+            // 
+            // FromPhoneNoLabel
+            // 
+            this.FromPhoneNoLabel.AutoSize = true;
+            this.FromPhoneNoLabel.Location = new System.Drawing.Point(262, 48);
+            this.FromPhoneNoLabel.Name = "FromPhoneNoLabel";
+            this.FromPhoneNoLabel.Size = new System.Drawing.Size(58, 13);
+            this.FromPhoneNoLabel.TabIndex = 12;
+            this.FromPhoneNoLabel.Text = "Phone No.";
+            // 
+            // MessageTextBox
+            // 
+            this.MessageTextBox.Location = new System.Drawing.Point(12, 332);
+            this.MessageTextBox.Multiline = true;
+            this.MessageTextBox.Name = "MessageTextBox";
+            this.MessageTextBox.Size = new System.Drawing.Size(479, 120);
+            this.MessageTextBox.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.OKButton);
+            this.panel1.Controls.Add(this.CloseButton);
+            this.panel1.Location = new System.Drawing.Point(497, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(85, 440);
+            this.panel1.TabIndex = 9;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.CloseButton.Location = new System.Drawing.Point(0, 417);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(85, 23);
+            this.CloseButton.TabIndex = 0;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            // 
+            // OKButton
+            // 
+            this.OKButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.OKButton.Location = new System.Drawing.Point(0, 394);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(85, 23);
+            this.OKButton.TabIndex = 1;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            // 
+            // NewMessageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 461);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.MessageTextBox);
+            this.Controls.Add(this.MessageFromGroupBox);
+            this.Controls.Add(this.MessageForGroupBox);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "NewMessageForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "New Message";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.MessageForGroupBox.ResumeLayout(false);
+            this.MessageForGroupBox.PerformLayout();
+            this.MessageFromGroupBox.ResumeLayout(false);
+            this.MessageFromGroupBox.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox MessageForGroupBox;
+        private System.Windows.Forms.Button LanguageButton;
+        private System.Windows.Forms.TextBox ForNameTextBox;
+        private System.Windows.Forms.TextBox LanguageTextBox;
+        private System.Windows.Forms.Label ForNameLabel;
+        private System.Windows.Forms.Label LanguageLabel;
+        private System.Windows.Forms.GroupBox MessageFromGroupBox;
+        private System.Windows.Forms.TextBox FromPhoneNoTextBox;
+        private System.Windows.Forms.Label FromPhoneNoLabel;
+        private System.Windows.Forms.Button FromTitleButton;
+        private System.Windows.Forms.TextBox FromTitleTextBox;
+        private System.Windows.Forms.Label FromTitleLabel;
+        private System.Windows.Forms.TextBox FromFirstTextBox;
+        private System.Windows.Forms.Label FromFirstLabel;
+        private System.Windows.Forms.TextBox FromNameTextBox;
+        private System.Windows.Forms.TextBox FromCompanyTextBox;
+        private System.Windows.Forms.Label FromNameLabel;
+        private System.Windows.Forms.Label FromCompanyLabel;
+        private System.Windows.Forms.TextBox MessageTextBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button OKButton;
+        private System.Windows.Forms.Button CloseButton;
+    }
+}
