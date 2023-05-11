328cfa0acf1bccccefb859c0da131d5b4306f719
Stariy <djplaymmen@gmail.com>
2023-05-11 22:21:36 +0800

    Черновой вариант Packages New с элементами

---
+++ HotelEDU/NewPackagesForm.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace HotelEDU
+{
+    partial class NewPackagesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewPackagesForm));
+            this.PackageLabel = new System.Windows.Forms.Label();
+            this.PackageTextBox = new System.Windows.Forms.TextBox();
+            this.PackageButton = new System.Windows.Forms.Button();
+            this.QuantityLabel = new System.Windows.Forms.Label();
+            this.QuantityTextBox = new System.Windows.Forms.TextBox();
+            this.ExcludeTextBox = new System.Windows.Forms.TextBox();
+            this.ExcludeLabel = new System.Windows.Forms.Label();
+            this.PostingRhythmLabel = new System.Windows.Forms.Label();
+            this.PostingRhythmTextBox = new System.Windows.Forms.TextBox();
+            this.CalculationRuleLabel = new System.Windows.Forms.Label();
+            this.CalculationRuleTextBox = new System.Windows.Forms.TextBox();
+            this.BeginDateLabel = new System.Windows.Forms.Label();
+            this.BeginDateTextBox = new System.Windows.Forms.TextBox();
+            this.EndDateLabel = new System.Windows.Forms.Label();
+            this.EndDateTextBox = new System.Windows.Forms.TextBox();
+            this.BeginDateCalendarButton = new System.Windows.Forms.Button();
+            this.EndDateCalendarButton = new System.Windows.Forms.Button();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PackageLabel
+            // 
+            this.PackageLabel.AutoSize = true;
+            this.PackageLabel.Location = new System.Drawing.Point(46, 17);
+            this.PackageLabel.Name = "PackageLabel";
+            this.PackageLabel.Size = new System.Drawing.Size(50, 13);
+            this.PackageLabel.TabIndex = 0;
+            this.PackageLabel.Text = "Package";
+            // 
+            // PackageTextBox
+            // 
+            this.PackageTextBox.Location = new System.Drawing.Point(102, 14);
+            this.PackageTextBox.Name = "PackageTextBox";
+            this.PackageTextBox.Size = new System.Drawing.Size(200, 20);
+            this.PackageTextBox.TabIndex = 1;
+            // 
+            // PackageButton
+            // 
+            this.PackageButton.Location = new System.Drawing.Point(308, 12);
+            this.PackageButton.Name = "PackageButton";
+            this.PackageButton.Size = new System.Drawing.Size(35, 23);
+            this.PackageButton.TabIndex = 2;
+            this.PackageButton.UseVisualStyleBackColor = true;
+            // 
+            // QuantityLabel
+            // 
+            this.QuantityLabel.AutoSize = true;
+            this.QuantityLabel.Location = new System.Drawing.Point(50, 43);
+            this.QuantityLabel.Name = "QuantityLabel";
+            this.QuantityLabel.Size = new System.Drawing.Size(46, 13);
+            this.QuantityLabel.TabIndex = 3;
+            this.QuantityLabel.Text = "Quantity";
+            // 
+            // QuantityTextBox
+            // 
+            this.QuantityTextBox.Location = new System.Drawing.Point(102, 40);
+            this.QuantityTextBox.Name = "QuantityTextBox";
+            this.QuantityTextBox.Size = new System.Drawing.Size(50, 20);
+            this.QuantityTextBox.TabIndex = 4;
+            // 
+            // ExcludeTextBox
+            // 
+            this.ExcludeTextBox.Location = new System.Drawing.Point(102, 66);
+            this.ExcludeTextBox.Name = "ExcludeTextBox";
+            this.ExcludeTextBox.Size = new System.Drawing.Size(50, 20);
+            this.ExcludeTextBox.TabIndex = 5;
+            // 
+            // ExcludeLabel
+            // 
+            this.ExcludeLabel.AutoSize = true;
+            this.ExcludeLabel.Location = new System.Drawing.Point(51, 69);
+            this.ExcludeLabel.Name = "ExcludeLabel";
+            this.ExcludeLabel.Size = new System.Drawing.Size(45, 13);
+            this.ExcludeLabel.TabIndex = 6;
+            this.ExcludeLabel.Text = "Exclude";
+            // 
+            // PostingRhythmLabel
+            // 
+            this.PostingRhythmLabel.AutoSize = true;
+            this.PostingRhythmLabel.Location = new System.Drawing.Point(15, 95);
+            this.PostingRhythmLabel.Name = "PostingRhythmLabel";
+            this.PostingRhythmLabel.Size = new System.Drawing.Size(81, 13);
+            this.PostingRhythmLabel.TabIndex = 7;
+            this.PostingRhythmLabel.Text = "Posting Rhythm";
+            // 
+            // PostingRhythmTextBox
+            // 
+            this.PostingRhythmTextBox.Location = new System.Drawing.Point(102, 92);
+            this.PostingRhythmTextBox.Name = "PostingRhythmTextBox";
+            this.PostingRhythmTextBox.Size = new System.Drawing.Size(241, 20);
+            this.PostingRhythmTextBox.TabIndex = 8;
+            // 
+            // CalculationRuleLabel
+            // 
+            this.CalculationRuleLabel.AutoSize = true;
+            this.CalculationRuleLabel.Location = new System.Drawing.Point(12, 121);
+            this.CalculationRuleLabel.Name = "CalculationRuleLabel";
+            this.CalculationRuleLabel.Size = new System.Drawing.Size(84, 13);
+            this.CalculationRuleLabel.TabIndex = 9;
+            this.CalculationRuleLabel.Text = "Calculation Rule";
+            // 
+            // CalculationRuleTextBox
+            // 
+            this.CalculationRuleTextBox.Location = new System.Drawing.Point(102, 118);
+            this.CalculationRuleTextBox.Name = "CalculationRuleTextBox";
+            this.CalculationRuleTextBox.Size = new System.Drawing.Size(241, 20);
+            this.CalculationRuleTextBox.TabIndex = 10;
+            // 
+            // BeginDateLabel
+            // 
+            this.BeginDateLabel.AutoSize = true;
+            this.BeginDateLabel.Location = new System.Drawing.Point(36, 147);
+            this.BeginDateLabel.Name = "BeginDateLabel";
+            this.BeginDateLabel.Size = new System.Drawing.Size(60, 13);
+            this.BeginDateLabel.TabIndex = 11;
+            this.BeginDateLabel.Text = "Begin Date";
+            // 
+            // BeginDateTextBox
+            // 
+            this.BeginDateTextBox.Location = new System.Drawing.Point(102, 144);
+            this.BeginDateTextBox.Name = "BeginDateTextBox";
+            this.BeginDateTextBox.Size = new System.Drawing.Size(100, 20);
+            this.BeginDateTextBox.TabIndex = 12;
+            // 
+            // EndDateLabel
+            // 
+            this.EndDateLabel.AutoSize = true;
+            this.EndDateLabel.Location = new System.Drawing.Point(44, 173);
+            this.EndDateLabel.Name = "EndDateLabel";
+            this.EndDateLabel.Size = new System.Drawing.Size(52, 13);
+            this.EndDateLabel.TabIndex = 13;
+            this.EndDateLabel.Text = "End Date";
+            // 
+            // EndDateTextBox
+            // 
+            this.EndDateTextBox.Location = new System.Drawing.Point(102, 170);
+            this.EndDateTextBox.Name = "EndDateTextBox";
+            this.EndDateTextBox.Size = new System.Drawing.Size(100, 20);
+            this.EndDateTextBox.TabIndex = 14;
+            // 
+            // BeginDateCalendarButton
+            // 
+            this.BeginDateCalendarButton.Location = new System.Drawing.Point(208, 142);
+            this.BeginDateCalendarButton.Name = "BeginDateCalendarButton";
+            this.BeginDateCalendarButton.Size = new System.Drawing.Size(35, 23);
+            this.BeginDateCalendarButton.TabIndex = 15;
+            this.BeginDateCalendarButton.UseVisualStyleBackColor = true;
+            // 
+            // EndDateCalendarButton
+            // 
+            this.EndDateCalendarButton.Location = new System.Drawing.Point(208, 168);
+            this.EndDateCalendarButton.Name = "EndDateCalendarButton";
+            this.EndDateCalendarButton.Size = new System.Drawing.Size(35, 23);
+            this.EndDateCalendarButton.TabIndex = 16;
+            this.EndDateCalendarButton.UseVisualStyleBackColor = true;
+            // 
+            // OKButton
+            // 
+            this.OKButton.Location = new System.Drawing.Point(187, 233);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(75, 23);
+            this.OKButton.TabIndex = 17;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(268, 233);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(75, 23);
+            this.CloseButton.TabIndex = 18;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            // 
+            // NewPackagesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 268);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.EndDateCalendarButton);
+            this.Controls.Add(this.BeginDateCalendarButton);
+            this.Controls.Add(this.EndDateTextBox);
+            this.Controls.Add(this.EndDateLabel);
+            this.Controls.Add(this.BeginDateTextBox);
+            this.Controls.Add(this.BeginDateLabel);
+            this.Controls.Add(this.CalculationRuleTextBox);
+            this.Controls.Add(this.CalculationRuleLabel);
+            this.Controls.Add(this.PostingRhythmTextBox);
+            this.Controls.Add(this.PostingRhythmLabel);
+            this.Controls.Add(this.ExcludeLabel);
+            this.Controls.Add(this.ExcludeTextBox);
+            this.Controls.Add(this.QuantityTextBox);
+            this.Controls.Add(this.QuantityLabel);
+            this.Controls.Add(this.PackageButton);
+            this.Controls.Add(this.PackageTextBox);
+            this.Controls.Add(this.PackageLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "NewPackagesForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Packages New";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label PackageLabel;
+        private System.Windows.Forms.TextBox PackageTextBox;
+        private System.Windows.Forms.Button PackageButton;
+        private System.Windows.Forms.Label QuantityLabel;
+        private System.Windows.Forms.TextBox QuantityTextBox;
+        private System.Windows.Forms.TextBox ExcludeTextBox;
+        private System.Windows.Forms.Label ExcludeLabel;
+        private System.Windows.Forms.Label PostingRhythmLabel;
+        private System.Windows.Forms.TextBox PostingRhythmTextBox;
+        private System.Windows.Forms.Label CalculationRuleLabel;
+        private System.Windows.Forms.TextBox CalculationRuleTextBox;
+        private System.Windows.Forms.Label BeginDateLabel;
+        private System.Windows.Forms.TextBox BeginDateTextBox;
+        private System.Windows.Forms.Label EndDateLabel;
+        private System.Windows.Forms.TextBox EndDateTextBox;
+        private System.Windows.Forms.Button BeginDateCalendarButton;
+        private System.Windows.Forms.Button EndDateCalendarButton;
+        private System.Windows.Forms.Button OKButton;
+        private System.Windows.Forms.Button CloseButton;
+    }
+}
