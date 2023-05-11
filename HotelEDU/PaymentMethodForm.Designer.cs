be3a31dd51da5897b12d956be2f09d60c77bc2ff
Stariy <djplaymmen@gmail.com>
2023-05-11 23:22:36 +0800

    Черновой вариант Paymene Method с элементами

---
+++ HotelEDU/PaymentMethodForm.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace HotelEDU
+{
+    partial class PaymentMethodForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PaymentMethodForm));
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.RoomLabel = new System.Windows.Forms.Label();
+            this.RoomTextBox = new System.Windows.Forms.TextBox();
+            this.RoomButton = new System.Windows.Forms.Button();
+            this.MethodOfPaymentButton1 = new System.Windows.Forms.Button();
+            this.MethodOfPaymentTextBox = new System.Windows.Forms.TextBox();
+            this.MethodOfPaymentLabel = new System.Windows.Forms.Label();
+            this.MethodOfPaymentButton2 = new System.Windows.Forms.Button();
+            this.CreditCardNoLabel = new System.Windows.Forms.Label();
+            this.CreditCardNoTextBox = new System.Windows.Forms.TextBox();
+            this.ExpirationDateLabel = new System.Windows.Forms.Label();
+            this.ExpirationDateTextBox = new System.Windows.Forms.TextBox();
+            this.AvailableRoomsLabel = new System.Windows.Forms.Label();
+            this.MethodOfPaymentTypeLabel = new System.Windows.Forms.Label();
+            this.AvailableRoomsNumsLabel = new System.Windows.Forms.Label();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(78, 15);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(35, 13);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Name";
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(119, 12);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(255, 20);
+            this.NameTextBox.TabIndex = 1;
+            // 
+            // RoomLabel
+            // 
+            this.RoomLabel.AutoSize = true;
+            this.RoomLabel.Location = new System.Drawing.Point(78, 41);
+            this.RoomLabel.Name = "RoomLabel";
+            this.RoomLabel.Size = new System.Drawing.Size(35, 13);
+            this.RoomLabel.TabIndex = 2;
+            this.RoomLabel.Text = "Room";
+            // 
+            // RoomTextBox
+            // 
+            this.RoomTextBox.Location = new System.Drawing.Point(119, 38);
+            this.RoomTextBox.Name = "RoomTextBox";
+            this.RoomTextBox.Size = new System.Drawing.Size(100, 20);
+            this.RoomTextBox.TabIndex = 3;
+            // 
+            // RoomButton
+            // 
+            this.RoomButton.Location = new System.Drawing.Point(225, 36);
+            this.RoomButton.Name = "RoomButton";
+            this.RoomButton.Size = new System.Drawing.Size(35, 23);
+            this.RoomButton.TabIndex = 4;
+            this.RoomButton.Text = "button1";
+            this.RoomButton.UseVisualStyleBackColor = true;
+            // 
+            // MethodOfPaymentButton1
+            // 
+            this.MethodOfPaymentButton1.Location = new System.Drawing.Point(225, 62);
+            this.MethodOfPaymentButton1.Name = "MethodOfPaymentButton1";
+            this.MethodOfPaymentButton1.Size = new System.Drawing.Size(35, 23);
+            this.MethodOfPaymentButton1.TabIndex = 7;
+            this.MethodOfPaymentButton1.Text = "button2";
+            this.MethodOfPaymentButton1.UseVisualStyleBackColor = true;
+            // 
+            // MethodOfPaymentTextBox
+            // 
+            this.MethodOfPaymentTextBox.Location = new System.Drawing.Point(119, 64);
+            this.MethodOfPaymentTextBox.Name = "MethodOfPaymentTextBox";
+            this.MethodOfPaymentTextBox.Size = new System.Drawing.Size(100, 20);
+            this.MethodOfPaymentTextBox.TabIndex = 6;
+            // 
+            // MethodOfPaymentLabel
+            // 
+            this.MethodOfPaymentLabel.AutoSize = true;
+            this.MethodOfPaymentLabel.Location = new System.Drawing.Point(12, 67);
+            this.MethodOfPaymentLabel.Name = "MethodOfPaymentLabel";
+            this.MethodOfPaymentLabel.Size = new System.Drawing.Size(101, 13);
+            this.MethodOfPaymentLabel.TabIndex = 5;
+            this.MethodOfPaymentLabel.Text = "Method Of Payment";
+            // 
+            // MethodOfPaymentButton2
+            // 
+            this.MethodOfPaymentButton2.Location = new System.Drawing.Point(266, 62);
+            this.MethodOfPaymentButton2.Name = "MethodOfPaymentButton2";
+            this.MethodOfPaymentButton2.Size = new System.Drawing.Size(35, 23);
+            this.MethodOfPaymentButton2.TabIndex = 8;
+            this.MethodOfPaymentButton2.Text = "button3";
+            this.MethodOfPaymentButton2.UseVisualStyleBackColor = true;
+            // 
+            // CreditCardNoLabel
+            // 
+            this.CreditCardNoLabel.AutoSize = true;
+            this.CreditCardNoLabel.Location = new System.Drawing.Point(34, 93);
+            this.CreditCardNoLabel.Name = "CreditCardNoLabel";
+            this.CreditCardNoLabel.Size = new System.Drawing.Size(79, 13);
+            this.CreditCardNoLabel.TabIndex = 9;
+            this.CreditCardNoLabel.Text = "Credit Card No.";
+            // 
+            // CreditCardNoTextBox
+            // 
+            this.CreditCardNoTextBox.Location = new System.Drawing.Point(119, 90);
+            this.CreditCardNoTextBox.Name = "CreditCardNoTextBox";
+            this.CreditCardNoTextBox.Size = new System.Drawing.Size(141, 20);
+            this.CreditCardNoTextBox.TabIndex = 10;
+            // 
+            // ExpirationDateLabel
+            // 
+            this.ExpirationDateLabel.AutoSize = true;
+            this.ExpirationDateLabel.Location = new System.Drawing.Point(34, 119);
+            this.ExpirationDateLabel.Name = "ExpirationDateLabel";
+            this.ExpirationDateLabel.Size = new System.Drawing.Size(79, 13);
+            this.ExpirationDateLabel.TabIndex = 11;
+            this.ExpirationDateLabel.Text = "Expiration Date";
+            // 
+            // ExpirationDateTextBox
+            // 
+            this.ExpirationDateTextBox.Location = new System.Drawing.Point(119, 116);
+            this.ExpirationDateTextBox.Name = "ExpirationDateTextBox";
+            this.ExpirationDateTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ExpirationDateTextBox.TabIndex = 12;
+            // 
+            // AvailableRoomsLabel
+            // 
+            this.AvailableRoomsLabel.AutoSize = true;
+            this.AvailableRoomsLabel.Location = new System.Drawing.Point(116, 152);
+            this.AvailableRoomsLabel.Name = "AvailableRoomsLabel";
+            this.AvailableRoomsLabel.Size = new System.Drawing.Size(86, 13);
+            this.AvailableRoomsLabel.TabIndex = 13;
+            this.AvailableRoomsLabel.Text = "Available Rooms";
+            // 
+            // MethodOfPaymentTypeLabel
+            // 
+            this.MethodOfPaymentTypeLabel.AutoSize = true;
+            this.MethodOfPaymentTypeLabel.Location = new System.Drawing.Point(307, 67);
+            this.MethodOfPaymentTypeLabel.Name = "MethodOfPaymentTypeLabel";
+            this.MethodOfPaymentTypeLabel.Size = new System.Drawing.Size(38, 13);
+            this.MethodOfPaymentTypeLabel.TabIndex = 14;
+            this.MethodOfPaymentTypeLabel.Text = "MOPT";
+            // 
+            // AvailableRoomsNumsLabel
+            // 
+            this.AvailableRoomsNumsLabel.AutoSize = true;
+            this.AvailableRoomsNumsLabel.Location = new System.Drawing.Point(116, 177);
+            this.AvailableRoomsNumsLabel.Name = "AvailableRoomsNumsLabel";
+            this.AvailableRoomsNumsLabel.Size = new System.Drawing.Size(110, 13);
+            this.AvailableRoomsNumsLabel.TabIndex = 15;
+            this.AvailableRoomsNumsLabel.Text = "AvailableRoomsNums";
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(299, 223);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.TabIndex = 17;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // OKButton
+            // 
+            this.OKButton.Location = new System.Drawing.Point(218, 223);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(75, 23);
+            this.OKButton.TabIndex = 18;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            // 
+            // PaymentMethodForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 255);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.AvailableRoomsNumsLabel);
+            this.Controls.Add(this.MethodOfPaymentTypeLabel);
+            this.Controls.Add(this.AvailableRoomsLabel);
+            this.Controls.Add(this.ExpirationDateTextBox);
+            this.Controls.Add(this.ExpirationDateLabel);
+            this.Controls.Add(this.CreditCardNoTextBox);
+            this.Controls.Add(this.CreditCardNoLabel);
+            this.Controls.Add(this.MethodOfPaymentButton2);
+            this.Controls.Add(this.MethodOfPaymentButton1);
+            this.Controls.Add(this.MethodOfPaymentTextBox);
+            this.Controls.Add(this.MethodOfPaymentLabel);
+            this.Controls.Add(this.RoomButton);
+            this.Controls.Add(this.RoomTextBox);
+            this.Controls.Add(this.RoomLabel);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.NameLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "PaymentMethodForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Payment Method";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Label RoomLabel;
+        private System.Windows.Forms.TextBox RoomTextBox;
+        private System.Windows.Forms.Button RoomButton;
+        private System.Windows.Forms.Button MethodOfPaymentButton1;
+        private System.Windows.Forms.TextBox MethodOfPaymentTextBox;
+        private System.Windows.Forms.Label MethodOfPaymentLabel;
+        private System.Windows.Forms.Button MethodOfPaymentButton2;
+        private System.Windows.Forms.Label CreditCardNoLabel;
+        private System.Windows.Forms.TextBox CreditCardNoTextBox;
+        private System.Windows.Forms.Label ExpirationDateLabel;
+        private System.Windows.Forms.TextBox ExpirationDateTextBox;
+        private System.Windows.Forms.Label AvailableRoomsLabel;
+        private System.Windows.Forms.Label MethodOfPaymentTypeLabel;
+        private System.Windows.Forms.Label AvailableRoomsNumsLabel;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Button OKButton;
+    }
+}
